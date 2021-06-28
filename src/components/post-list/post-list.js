41c5db4670abde4f4612964d9bd232a77cb294e1
Sadigzade Hikmet <sadigzade.hi@mail.ru>
2021-06-28 15:36:11 +0300

    Second commit

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,15 +5,16 @@ import { ListGroup } from 'reactstrap';
 
 import './post-list.css';
 
-const PostList = ({posts, onDelete}) => {
-
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
   const elements = posts.map((item) => {
     const {id, ...itemProps} = item;
     return (
       <li key={id} className="list-group-item">
         <PostListItem
           {...itemProps}
-          onDelete={() => onDelete(id)} />
+          onDelete={() => onDelete(id)}
+          onToggleImportant={() => onToggleImportant(id)}
+          onToggleLiked={() => onToggleLiked(id)}/>
       </li>
     )
   }); 
